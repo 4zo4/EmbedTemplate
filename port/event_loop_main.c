99fc187a5e07ec4780db23fa6b0a53050e524c5d
4zo4 <29783388+4zo4@users.noreply.github.com>
2026-08-01 13:23:32 -0700

    update event_loop_main.c to reflect framework

--- port/event_loop_main.c
+++ port/event_loop_main.c
@@ -1,35 +1,31 @@
 /**
- * @file posix_main.c
- * @brief Main for posix build.
+ * @file event_loop_main.c
+ * @brief Main for event loop build.
  * This file contains the main function for the non-RTOS build. Without an RTOS, the main function is an event loop.
- * On bare metal, the event loop handles events from hardware interrupts.
- * On a POSIX system, the event loop is driven by the CLI input.
+ * On bare metal, the event loop handles events from hardware interrupts. On a user-space POSIX system,
+ * the event loop is driven by the CLI input.
  */
 #include <stdint.h>
 #include <unistd.h>
 
 #include "arch_ops.h"
 #include "event.h"
-#include "gpio_demo_regs.h"
-#include "gpio.h"
-#include "log.h"
-#include "log_marker.h"
 #include "utils.h"
 
 // prototypes without include file
-int      cli_init(void **cli_ctx);
-bool     cli_run(void *cli_ctx);
-void     cli_exit(void *cli_ctx);
-void     init_pci(void);
-void     init_uart(void);
-void     init_systick(void);
-void     init_timestamp(void);
+int  init_hw(void);
+int  init_gpio(void);
+int  cli_init(void **cli_ctx);
+bool cli_run(void *cli_ctx);
+void cli_exit(void *cli_ctx);
+void test_pci(void);
+void test_pci_msi_irq(int test_idx);
+void test_pci_post_msi_irq(int test_idx);
+
 uint64_t get_timestamp48(void);
-void     init_watchdog(void);
 
 // -- Globals --
 
-extern void (*volatile init_port_globals)(void);
 extern volatile bool keep_running;
 
 // -- End of globals --
@@ -51,57 +47,28 @@ int main(void)
 {
     void *cli_ctx;
 
-    alignas(8) static volatile gpio_ctrl_t gpio; // Local instance of GPIO registers -
-                                                 // simulated memory-mapped hardware
-#ifdef BARE_METAL
-    if (init_port_globals)
-        init_port_globals();
-    init_timestamp();
-    init_systick();
-    init_uart();
-    init_watchdog();
-#ifdef ENABLE_PCI
-    init_pci();
+    init_hw();         // initialize hardware
+    get_timestamp48(); // start time
+#ifdef ENABLE_TEST
+    init_gpio();
 #endif
-#endif
-    get_timestamp48();    // start time
-    gpio_set_regs(&gpio); // Set the base address for the GPIO driver
-    // Set log level for gpio
-    log_set_level(DOMAIN_DEV, ENTITY_GPIO, LOG_LEVEL_INFO);
-
-    // Initialize the GPIO Controller to a known safe state
-    gpio_init_controller(&gpio);
-
-    // Configure Pin 0: Heater (Output), Pin 1: Cooler (Output),
-    //           Pin 2: Emergency Cooler (Output),
-    //           Pin 0-7: Sensor Inputs (Temperature), Pin 8: Alarm Input
-    gpio_init_pin(&gpio, 0, true);
-    gpio_init_pin(&gpio, 1, true);
-    gpio_init_pin(&gpio, 2, true);
-    gpio_init_pin(&gpio, 8, false);
-    gpio_init_pin_mask(&gpio, 0xFF, false);
-
-    // Configure and Enable Interrupt for the Alarm Pin
-    gpio_configure_interrupt(&gpio, 8, false); // Active High
-    // Clear any stale status from the init/boot period
-    gpio_clear_interrupt(&gpio, 8);
-    // Configure Watchdog with a 150ms timeout
-    gpio_wdt_setup(&gpio, 150);
-
     if (cli_init(&cli_ctx))
         return 0;
 
     while (keep_running) {
 #ifdef BARE_METAL
-        uint32_t primask = disable_interrupts();
-        uint32_t event = event_notify;
-        event_notify = 0; // Clear the events
-        restore_interrupts(primask);
+        uint32_t event = __atomic_exchange_n(&event_notify, 0, __ATOMIC_SEQ_CST);
 
         if (event) {
-            if (event & EVT_MSI_MASK) { // is PCI MSI event
-                NOP();                  // Placeholder for tasks that need to run on PCI MSI event
+#ifdef ENABLE_PCI
+            if (event & EVT_PCI_TEST) { // is PCI Test event
+                test_pci();             // start PCI test (1)
             }
+            if (event & EVT_MSI_MASK) {   // is PCI MSI event
+                test_pci_post_msi_irq(2); // MSI test epilogue (2.2)
+                NOP();                    // Placeholder for tasks that need to run on PCI MSI event
+            }
+#endif
             if (event & EVT_SYS_TICK) { // is SysTick event
                 NOP();                  // Placeholder for tasks that need to run on SysTick
             }
@@ -109,8 +76,13 @@ int main(void)
                 NOP();                    // CLI passthrough for data pending and read from UART
             }
             keep_running = cli_run(cli_ctx);
+#ifdef ENABLE_PCI
+            if (event & EVT_PCI_TEST) {
+                test_pci_msi_irq(2); // start PCI MSI test (2.1) after flashing PCI Test logs
+            }
+#endif
         }
-#else
+#else // !BARE_METAL
         keep_running = cli_run(cli_ctx);
 #endif
         idle();
